96ca06acdd9083583e6de6e68023ef4042d4bcce
Felipe Klerk Signorini <felipe.signorini@b2wdigital.com>
2017-11-24 03:01:36 -0200

    version base

--- app/controller/discovery.py
+++ app/controller/discovery.py
@@ -1,9 +1,11 @@
 
+import os, json
+from pydash.objects import pick
 from flask_restful import Resource
 
 class DiscoveryApp(Resource):
     def get(self):
-        return {
-            'name': 'Discovery API',
-            'version': '0.10'
-        }
+        fn = os.path.dirname(os.path.abspath(__file__))
+        json_data = open(fn+'/../../package.json').read()
+        data = json.loads(json_data)
+        return pick(data, ['name', 'provider', 'description', 'version', 'license'])
