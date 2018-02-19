97f2f49b39b848d6ca13858c3de7062a67890c90
Felipe Signorini <fsignorini@rdisoftware.com>
2018-02-19 22:09:00 +0000

    bugfix json encoder

--- app/libs/jsonEncoder.py
+++ app/libs/jsonEncoder.py
@@ -8,7 +8,9 @@ class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
-        if isinstance(obj, (datetime.datetime, datetime.date)):
+        if isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
             return obj.isoformat()
-        
+        if isinstance(obj, datetime.timedelta):
+            return (datetime.datetime.min + obj).time().isoformat()
+
         return json.JSONEncoder.default(self, obj)
