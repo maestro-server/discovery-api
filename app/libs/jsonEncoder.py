c11a3147981a5b93be1d9329cc4b43b80a985ff3
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-02-20 01:03:33 -0300

    several bugfix - docker file

--- app/libs/jsonEncoder.py
+++ app/libs/jsonEncoder.py
@@ -7,8 +7,10 @@ class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, ObjectId):
             return str(obj)
-        if isinstance(obj, (timestamp.Timestamp, datetime.datetime, datetime.date, datetime.time)):
+        if isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
             return obj.isoformat()
+        if isinstance(obj, timestamp.Timestamp):
+            return str(obj)
         if isinstance(obj, datetime.timedelta):
             return (datetime.datetime.min + obj).time().isoformat()
 
