c56de0b3fbf7ad01a996f1782f563fa6ae68eed3
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-04-15 18:51:44 -0300

    pip requirements

--- app/app.py
+++ app/app.py
@@ -5,21 +5,9 @@ Licence: GPLv3
 """
 
 from flask import Flask
-import pymongo
-from pymongo import MongoClient
 from .celery import make_celery
 
 app = Flask(__name__)
 app.config.from_object('instance.config.Config')
 
-client = MongoClient(app.config['DATABASE_URI'], serverSelectionTimeoutMS=1)
-db = client[app.config['DATABASE_NAME']]
-
 celery = make_celery(app)
-
-
-try:
-    client.server_info() # Forces a call.
-    print("Mongo Online")
-except pymongo.errors.ServerSelectionTimeoutError as err:
-    print("==================================> MongoDB is down", err)
--- instance/config.py
+++ instance/config.py
@@ -7,28 +7,21 @@ Licence: GPLv3
 import os
 from app.libs.jsonEncoder import DateTimeEncoder
 from dotenv import load_dotenv, find_dotenv
-from kombu import Queue
 
 load_dotenv(find_dotenv())
-
 
 class Config(object):
     TESTING = os.environ.get("TESTING", False)
     SECRETJWT = os.environ.get("MAESTRO_SECRETJWT", "defaultSecretKey")
-    DATABASE_URI = "mongodb://" + os.environ.get("MAESTRO_MONGO_URI", "localhost")
-    DATABASE_NAME = os.environ.get("MAESTRO_MONGO_DATABASE", "maestro-client")
     RESTFUL_JSON = {'cls': DateTimeEncoder}
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", 'amqp://localhost')
     CELERY_DEFAULT_QUEUE = 'discovery'
 
-
 class ProductionConfig(Config):
     pass
 
-
 class DevelopmentConfig(Config):
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
