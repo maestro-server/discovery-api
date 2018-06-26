a9ce766f1e516e39555dc6b572319d3f3275f0e8
fsignorini <fsignorini@rdisoftware.com>
2018-06-26 15:12:23 +0000

    change log to tracker task - show list content

--- app/tasks/tracker.py
+++ app/tasks/tracker.py
@@ -7,13 +7,13 @@ from app.libs.url import FactoryURL
 @celery.task(name="tracker.api")
 def task_tracker(result, dc_id, region, task):
 
-    ids = map(lambda x: get(x, 'unique_id'), result)
+    ids = list(map(lambda x: get(x, 'unique_id'), result))
     region = slugify(region)
 
     body = [{
         '_id': dc_id,
         '$addToSet': {
-            "tracker.%s.%s" % (task, region): {'$each': list(ids)}
+            "tracker.%s.%s" % (task, region): {'$each': ids}
         }
     }]
 
